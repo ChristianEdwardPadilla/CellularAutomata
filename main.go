edf17378fa0edbcd379183453a2a38b4628a884a
ChristianEdwardPadilla <christianepadilla@gmail.com>
2025-01-25 11:34:44 -0500

    Add golang server wrapper

---
+++ main.go
@@ -0,0 +1,17 @@
+ckage main
+
+import (
+    "log"
+    "net/http"
+)
+
+func main() {
+    fs := http.FileServer(http.Dir("static"))
+    http.Handle("/", fs)
+
+    port := ":8080"
+    log.Printf("Server starting on port %s", port)
+    if err := http.ListenAndServe(port, nil); err != nil {
+        log.Fatal(err)
+    }
+}
